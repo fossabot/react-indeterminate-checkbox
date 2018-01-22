cf2ecf0d43f53e4038a154e153b6c26c21f19932
Michele Riva <master@micheleriva.it>
2018-01-22 15:19:08 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,26 +1,48 @@
-# React Indeterminable Checkbox
+# 🌕 🌗 🌑 React Indeterminable Checkbox 🌑 🌗  🌕
 
-[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](LICENSE)
 [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 [![Dependencies](https://david-dm.org/micheleriva/react-indeterminate-checkbox.svg)](https://david-dm.org/micheleriva/react-indeterminate-checkbox)
 [![Maintainability](https://api.codeclimate.com/v1/badges/76e16f7e4ea426c0734b/maintainability)](https://codeclimate.com/github/micheleriva/react-indeterminate-checkbox/maintainability)
 
 Simple checkbox component with "indeterminate" status support.
 
+### Live Demo
+[Live demo here!](https://codesandbox.io/s/2z72q9k87r)
+
 ### Installation
 
+You can install this package with `npm` or `yarn`
+
 ```bash
-npm install --save react-indeterminate-checkbox
+npm install react-indeterminate-checkbox
+```
+
+```bash
+yarn add react-indeterminate-checkbox
 ```
 
 ### Usage
 
 ```jsx
-<IndeterminableCheckbox
-   change={val => console.log(val)}
-   name="mycheckbox-name"
-   indeterminate
-/>
+import IndeterminableCheckbox from 'react-indeterminate-checkbox'
+
+const checkBox = () => {
+
+    <IndeterminableCheckbox
+       change={val => console.log(val)}
+       name="mycheckbox-name"
+       indeterminate={true}
+    />
+
+}
+
 ```
 
-Tests will be added soon!
+### Props
+
+| Name | Value |
+| ---- | ----- |
+| `change` | Returns `0`, `1` or `2`. <br /> `0`: indeterminable <br /> `1`: checked <br /> `2`: unchecked |
+| `name` | the checkbox input name |
+| `indeterminate` | Boolean value, set `true` if you want to start with an indeterminable status. <br /> Set to `false` if you want it to start with the checked value. |
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ import PropTypes from 'prop-types'
 
 export default class IndeterminableCheckbox extends React.Component {
   constructor (props) {
-    super(props)
+    super(props);
     this.state = {
       ...this.props,
       status: this.props.indeterminate ? 0 : 1
@@ -40,9 +40,9 @@ export default class IndeterminableCheckbox extends React.Component {
   }
 
   changeStatus () {
-    const status = this.state.status
+    const status = this.state.status;
     if (status < 2) {
-      let newState = status + 1
+      let newState = status + 1;
       return this.setState({status: newState}, () => this.handleChangeProp())
     } else {
       return this.setState({status: 0}, () => this.handleChangeProp())
@@ -54,17 +54,17 @@ export default class IndeterminableCheckbox extends React.Component {
 
     switch (status) {
       case 1:
-        this.el.indeterminate = false
-        this.el.checked = true
-        break
+        this.el.indeterminate = false;
+        this.el.checked = true;
+        break;
       case 2:
-        this.el.indeterminate = false
-        this.el.checked = false
-        break
+        this.el.indeterminate = false;
+        this.el.checked = false;
+        break;
       default:
-        this.el.indeterminate = true
-        this.el.checked = false
-        break
+        this.el.indeterminate = true;
+        this.el.checked = false;
+        break;
     }
 
     return this.props.change(status)
@@ -77,7 +77,7 @@ export default class IndeterminableCheckbox extends React.Component {
         type='checkbox'
         ref={el => this.el = el}
         onChange={() => this.changeStatus()}
-            />
+      />
     )
   }
 }
@@ -86,8 +86,8 @@ IndeterminableCheckbox.propTypes = {
   indeterminate: PropTypes.bool,
   change: PropTypes.func,
   name: PropTypes.string
-}
+};
 
 IndeterminableCheckbox.defaultProps = {
   indeterminate: true
-}
+};
